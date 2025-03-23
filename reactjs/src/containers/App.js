54e7c4f74a936ff9dd08726182a351d56ad4ea1b
vongocanh03 <148541157+vongocanh03@users.noreply.github.com>
2025-03-23 13:02:29 +0700

    hoan thanh giao dien va chuyen doi ngon ngu

--- reactjs/src/containers/App.js
+++ reactjs/src/containers/App.js
@@ -19,6 +19,7 @@ import System from '../routes/System';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
 import ConfirmModal from '../components/ConfirmModal';
+import HomePage from './HomePage/HomePage';
 
 class App extends Component {
 
@@ -53,6 +54,7 @@ class App extends Component {
                                 <Route path={path.HOME} exact component={(Home)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                <Route path={path.HOMEPAGE} component={HomePage}/>
                             </Switch>
                         </span>
 
